402d410f7c65daaa9f38ae8c59ea8c1526f811fe
diegoantonioalmontezarzuela@gmail.com <diegoantonioalmontezarzuela@gmail.com>
2018-03-14 11:15:29 +0100

    11.Añadido el metodo eat

--- Game.java
+++ Game.java
@@ -122,6 +122,9 @@ public class Game
         else if (commandWord.equals("look")) {
             look();
         }
+        else if (commandWord.equals("eat")) {
+            eat();
+        }
 
         return wantToQuit;
     }
@@ -197,5 +200,10 @@ public class Game
     private void look() 
     {
         System.out.println(currentRoom.getLongDescription());
+    }
+    
+    private void eat()
+    {
+        System.out.println("You have eaten now and you are not hungry any more");
     }
 }
